885c01899bd0b85db7b79d399f43cdc207952cd7
Marjan Georgiev <marjan.georgiev@gmail.com>
2020-01-31 23:39:25 +0100

    Add yaml and typescript

--- metadata.js
+++ metadata.js
@@ -12,6 +12,19 @@
     },
     PLUGINS: [
       {
+        name: "monaco-typescript",
+        contrib: "vs/language/typescript/monaco.contribution",
+        modulePrefix: "vs/language/typescript",
+        thirdPartyNotices:
+          "node_modules/monaco-typescript/ThirdPartyNotices.txt",
+        paths: {
+          src: "/monaco-typescript/release/dev",
+          "npm/dev": "node_modules/monaco-typescript/release/dev",
+          "npm/min": "node_modules/monaco-typescript/release/min",
+          esm: "node_modules/monaco-typescript/release/esm"
+        }
+      },
+      {
         name: "monaco-yaml",
         contrib: "vs/language/yaml/monaco.contribution",
         modulePrefix: "vs/language/yaml",
